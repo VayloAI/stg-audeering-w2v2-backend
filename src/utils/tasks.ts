d054c11d020127b177828f1c68066982e41b9ff4
Toil <62353659+ilyhalight@users.noreply.github.com>
2025-03-12 15:27:39 +0300

    chore: added softmax

---
+++ src/utils/tasks.ts
@@ -0,0 +1,6 @@
+export function softmax(arr: Float32Array) {
+  const max = Math.max(...arr);
+  const exps = arr.map((v) => Math.exp(v - max));
+  const sum = exps.reduce((a, b) => a + b, 0);
+  return exps.map((v) => v / sum);
+}
